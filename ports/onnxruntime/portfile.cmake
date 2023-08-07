7d84524d6cd343a4ddc83bafe38de1d4a83d3c7f
Silvio Traversaro <silvio@traversaro.it>
2023-08-07 12:18:44 +0200

    Update portfile.cmake

--- ports/onnxruntime/portfile.cmake
+++ ports/onnxruntime/portfile.cmake
@@ -92,7 +92,10 @@ vcpkg_cmake_configure(
         -Donnxruntime_ENABLE_BITCODE=${VCPKG_TARGET_IS_IOS}
         -Donnxruntime_ENABLE_PYTHON=OFF
         -Donnxruntime_ENABLE_EXTERNAL_CUSTOM_OP_SCHEMAS=OFF
-        -DCMAKE_CUDA_ARCHITECTURES=all
+        # This are hardcoded architectures for our specific use case, to compile for 
+        # all comment this line and uncomment the following one
+        -DCMAKE_CUDA_ARCHITECTURES="75;86"
+        # -DCMAKE_CUDA_ARCHITECTURES=all
     OPTIONS_DEBUG
         -Donnxruntime_ENABLE_MEMLEAK_CHECKER=OFF
         -Donnxruntime_ENABLE_MEMORY_PROFILE=OFF
